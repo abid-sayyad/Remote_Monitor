c5ecbf58d3972cf24757cb7a9775da77ca75bf03
Abid <49099853+abid-sayyad@users.noreply.github.com>
2023-02-22 15:22:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser")
 const mongoose = require("mongoose")
 const dotenv = require("dotenv")
 
-const PORT = process.env.PORT || 3000
+const PORT = 80
 
 const app = express()
 
